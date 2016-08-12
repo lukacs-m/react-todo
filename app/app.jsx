9125cca0c8336f43dac35936f87d29277d3551af
martin <martin.lukacs@epitech.eu>
2016-08-12 17:42:07 +0200

    add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,19 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
 var TodoApp = require('TodoApp');
+
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(() => {
+    console.log('New state', store.getState());
+});
+
+store.dispatch(actions.addTodoItem('clean the yeard'));
+store.dispatch(actions.setSearchText('year'));
+store.dispatch(actions.toggleShowCompleted());
 
 // Load foundation
 $(document).foundation();
